5c97efc5b2ffbdabc7e1c7a0adcbd7268ec8b9c1
MKraemer <moritzk2@illinois.edu>
2019-04-26 20:44:43 -0500

    buncha buggy parsing

---
+++ app/src/main/java/com/example/finalprojectactual/Question.java
@@ -0,0 +1,34 @@
+package com.example.finalprojectactual;
+
+import java.io.ByteArrayInputStream;
+import org.w3c.dom.*;
+import java.io.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class Question extends MainActivity {
+    /**
+     * Query is the question
+     * options are all 4 answers so use these for buttons
+     * rightOne is for the function so dont use it.
+     */
+    private String Query;
+    private String[] options;
+    private String rightOne;
+    public Question(String xmlFromCarrot) {
+        Query = xmlFromCarrot;
+        options = null;
+        rightOne = "Carrot is scared of screwdrivers";
+        if (xmlFromCarrot == null) {
+            return;
+        }
+        DocumentBuilderFactory build = DocumentBuilderFactory.newInstance();
+        DocumentBuilder doc = build.newDocumentBuilder();
+        StringBuilder build2 = new StringBuilder();
+        build2.append("<?xml version= 1.0?> <class> </class>");
+        ByteArrayInputStream carrot = new ByteArrayInputStream(build2.toString().getBytes("UTF-8"));
+        Document dokument = doc.parse(carrot);
+        Element root = dokument.getDocumentElement();
+        // from here we can extract stuff apparently.
+    }
+}
